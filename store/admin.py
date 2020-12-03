445a361e3c7db817a75c89e35ebb6b1107109735
Devendra Singh <sdev6631@gmail.com>
2020-12-03 18:35:06 +0530

    Add index view, template and products

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Product, Category
 
+
+class AdminProduct(admin.ModelAdmin):
+     list_display = ['name', 'price', 'category']
 # Register your models here.
+
+class AdminCategory(admin.ModelAdmin):
+    list_display = ['name']
+
+admin.site.register(Product, AdminProduct)
+admin.site.register(Category, AdminCategory)
--- store/models.py
+++ store/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30, default='')
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.IntegerField(null=True)
+    description = models.CharField(max_length=1000, default='')
+    image = models.ImageField(upload_to='product/')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+
