f527526bd7fc4c576db707edbd924bfaa44fd3dd
Devendra Singh <sdev6631@gmail.com>
2020-12-04 19:24:58 +0530

    Update templates and views
    Add filtering login in index view.
    Add signup url, view and template.
    add login url and view.

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, signUp, login
 
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('signup/', signUp),
+    path('login/', login)
 ]
--- store/views.py
+++ store/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 def index(request):
+    category_id = request.GET.get('category')
 
-    products = Product.objects.all()
-    return render(request, 'index.html', {'products': products})
+    if category_id:
+        if category_id == 'all':
+            products = Product.objects.all()
+        else:
+            products = Product.objects.filter(category=category_id)
+    else:
+        products = Product.objects.all()
+
+    categories = Category.objects.all()
+    data = {}
+    data['products'] = products
+    data['categories'] = categories
+    return render(request, 'index.html', data)
+
+def signUp(request):
+
+    return render(request, 'signUp.html')
+
+def login(request):
+
+    return render(request, 'login.html')
