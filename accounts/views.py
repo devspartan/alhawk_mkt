cd48d1fe71f5f8df465517c8b192adfd2c49740c
Devendra Singh <sdev6631@gmail.com>
2020-12-07 13:35:18 +0530

    Add error message and updae validation

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,10 @@
+from django.contrib import messages
 from django.shortcuts import render, redirect
+
 from .forms import CreateUserForm
 from .models import User
-from django.core.mail import send_mail
 
-from django.contrib import messages
+
 # Create your views here.
 
 def signUp(request):
@@ -21,24 +22,27 @@ def signUp(request):
             phone_num = form_data.cleaned_data['phone_num']
             password = form_data.cleaned_data['password']
             confirm_password = form_data.cleaned_data['confirm_password']
-
             if_user = User.objects.filter(email=email)
+
             if len(if_user):
                 print("hey man im in registered")
                 messages.error(request, "This email is already registered")
             else:
-                messages.success(request, "Account Creates succesfully")
-                return redirect('store:index')
+                if password != confirm_password:
+                    messages.error(request, "Password doesn't match")
+                else:
+                    messages.success(request, "Account Created succesfully")
+                    return redirect('store:index')
 
-            return render(request, 'signUp.html', {'form': form})
         else:
-            print("im in out")
-            return render(request, 'signUp.html', {'form': form})
+            messages.error(request, "Enter valid phone number")
+
+        return render(request, 'signUp.html', {'form': form_data})
+
     else:
-        form = CreateUserForm()
-        return render(request, 'signUp.html', {'form': form})
+        form_data = CreateUserForm()
+    return render(request, 'signUp.html', {'form': form_data})
 
 
 def login(request):
-
     return render(request, 'login.html')
